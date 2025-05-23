917756c9bfc74e4e626733154492c883b12b4e47
Bottle414 <164286466+Bottle414@users.noreply.github.com>
2025-05-23 14:51:11 +0800

    feat: add basic styles and global scss variables

---
+++ src/router/index.ts
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/work',
+      name: 'work',
+      component: () => import('@/views/Work.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('@/views/About.vue')
+    }
+  ],
+})
+
+export default router
